cf9f36a270980575c2253ea7d82b1208cc2e7352
loremdev2 <singanamalaaman@gmail.com>
2025-05-03 09:43:38 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col items-center'>
+      <h1>Hello World</h1>
+    </div>
+      <Button variant="default" size="lg">
+        Button-1
+      </Button>
+    </>
+  )
+}
+
+export default App
